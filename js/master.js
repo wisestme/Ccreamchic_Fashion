88300974eaeee85a0b25b18d2abe63b5cb988550
wisestMe <wyhzest@gmail.com>
2020-05-02 15:54:21 +0100

    add hamburger modal function

--- js/master.js
+++ js/master.js
@@ -1,12 +1,30 @@
 let phoneIcon = document.querySelector('.phone_icon');
 let modal = document.querySelector('.modal');
-let close = document.querySelector('.close');
+let closeIcon = document.querySelector('.close');
+let hamburgerIcon = document.querySelector('.fa-bars');
+let hamburgerModal = document.querySelector('.hamburger_modal');
+let overlay = document.querySelector('.overlay');
+let hamburgerCloseIcon = document.querySelector('.hamburger_close');
+
+
 
 
 phoneIcon.addEventListener('click', () => {
 	modal.style.right = '0';
 });
 
-close.addEventListener('click', () => {
+closeIcon.addEventListener('click', () => {
 	modal.style.right = '-300px';
-})
+});
+
+hamburgerIcon.addEventListener('click', () => {
+	hamburgerModal.style.right = '0';
+	overlay.style.right = '0';
+	phoneIcon.style.display = 'none';
+});
+
+hamburgerCloseIcon.addEventListener('click', () => {
+	hamburgerModal.style.right = '-500px';
+	overlay.style.right = '-500px';
+	phoneIcon.style.display = 'flex';
+});
